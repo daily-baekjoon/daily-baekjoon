783bd18f246d29a63d8a7ece246acae0441b0159
Eunnyeong <doseee114@gmail.com>
2023-06-15 21:52:32 +0900

    feat: backend setting

---
+++ Backend/src/test/java/com/dailybaekjoon/DailyBaekjoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailybaekjoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyBaekjoonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
